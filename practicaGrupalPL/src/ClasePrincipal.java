e1533d6e4f379bc58e9c80a13bcbf1d192a1c458
Xangus9 <57356510+Xangus9@users.noreply.github.com>
2020-03-16 20:43:45 +0100

    Add files via upload

---
+++ practicaGrupalPL/src/ClasePrincipal.java
@@ -0,0 +1,63 @@
+import errors.MyErrorStrategy;
+import errors.TestE_Listener;
+import org.antlr.v4.runtime.*;
+
+import java.io.IOException;
+
+/*
+El nombre ClasePrincipal es arbitrario, escoge el que prefieras.
+Sustituye Hello por el nombre del fichero que contiene la especificación de la gramática ANTLR (extensión .g4)
+*/
+public class ClasePrincipal {
+    public static void main(String[] args) {
+        try{
+// Preparar el fichero de entrada para asignarlo al analizador léxico
+            CharStream input = CharStreams.fromFileName(args[0]);
+// Crear el objeto correspondiente al analizador léxico con el fichero de entrada
+            grupalLexer analex = new grupalLexer(input);
+
+            //PARA LOS ERRORES
+            analex.removeErrorListeners ();
+            analex.addErrorListener (new TestE_Listener.VerboseListener ());
+
+// Identificar al analizador léxico como fuente de tokens para el sintactico
+            CommonTokenStream tokens = new CommonTokenStream (analex);
+// Crear el objeto correspondiente al analizador sintáctico
+            grupalParser anasint = new grupalParser(tokens);
+
+            //PARA LOS ERRORES
+            anasint.removeErrorListeners ();
+            anasint.setErrorHandler (new BailErrorStrategy());
+            anasint.addErrorListener (new TestE_Listener.VerboseListener ());
+
+            //anasint.setErrorHandler (new MyErrorStrategy ());
+
+            //--------------------------------------------------------------------------------
+            /*class BailSimpleLexer extends grupalLexer {
+                public BailSimpleLexer(CharStream input) { super(input); }
+                public void recover(LexerNoViableAltException e) {
+                    throw new RuntimeException(e); // Bail out
+                }
+            }*/
+            //--------------------------------------------------------------------------------
+
+
+/*
+Si se quiere pasar al analizador algún objeto externo con el que trabajar, este deberá ser de una clase del mismo paquete
+Aquí se le llama "sintesis", pero puede ser cualquier nombre.
+HelloParser anasint = new HelloParser(tokens, new sintesis());
+*/
+/*
+Comenzar el análisis llamando al axioma de la gramática
+Atención, sustituye "AxiomaDeLaGramatica" por el nombre del axioma de tu gramática
+*/
+            anasint.prg();
+        } catch (org.antlr.v4.runtime.RecognitionException e) { //Fallo al reconocer la entrada
+            System.err.println("REC " + e.getMessage());
+        } catch (IOException e) { //Fallo de entrada/salida
+            System.err.println("IO " + e.getMessage());
+        } catch (RuntimeException e) { //Cualquier otro fallo
+            System.err.println("RUN " + e.getMessage());
+        }
+    }
+}
---
+++ practicaGrupalPL/src/errors/MyErrorStrategy.java
@@ -0,0 +1,61 @@
+package errors;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.IntervalSet;
+
+public class MyErrorStrategy extends DefaultErrorStrategy {
+    @Override
+    public void reportNoViableAlternative(Parser parser,
+                                          NoViableAltException e)
+            throws RecognitionException {
+        // ANTLR generates Parser subclasses from grammars and
+        // Parser extends Recognizer. Parameter parser is a
+        // pointer to the parser that detected the error
+        String msg = "no se puede elegir entre las alternativas disponibles";
+        parser.notifyErrorListeners (e.getOffendingToken (), msg, e);
+    }
+    @Override
+    protected void reportInputMismatch(Parser recognizer, InputMismatchException e) {
+        String msg = "se detectó un " + this.getTokenErrorDisplay(e.getOffendingToken()) + " y se esperaba un " + e.getExpectedTokens().toString(recognizer.getVocabulary());
+        recognizer.notifyErrorListeners(e.getOffendingToken(), msg, e);
+    }
+
+    @Override
+    protected void reportUnwantedToken(Parser recognizer) {
+        if (!this.inErrorRecoveryMode(recognizer)) {
+            this.beginErrorCondition(recognizer);
+            Token t = recognizer.getCurrentToken();
+            String tokenName = this.getTokenErrorDisplay(t);
+            IntervalSet expecting = this.getExpectedTokens(recognizer);
+            String msg = "se ha recibido " + tokenName + " pero se esperaba recibir " + expecting.toString(recognizer.getVocabulary());
+            recognizer.notifyErrorListeners(t, msg, (RecognitionException)null);
+        }
+    }
+
+    @Override
+    protected void reportMissingToken(Parser recognizer) {
+        if (!this.inErrorRecoveryMode(recognizer)) {
+            this.beginErrorCondition(recognizer);
+            Token t = recognizer.getCurrentToken();
+            IntervalSet expecting = this.getExpectedTokens(recognizer);
+            String msg = "¡vaya! estaba esperando recibir " + expecting.toString(recognizer.getVocabulary()) + " en " + this.getTokenErrorDisplay(t);
+            recognizer.notifyErrorListeners(t, msg, (RecognitionException)null);
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public void recover(Parser recognizer, RecognitionException e) {
+
+        throw (e);
+    }
+
+    @Override
+    public void sync(Parser recognizer) throws RecognitionException { }
+
+    @Override
+    public Token recoverInline(Parser recognizer) throws RecognitionException {
+        throw new RuntimeException(new InputMismatchException(recognizer));
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}
