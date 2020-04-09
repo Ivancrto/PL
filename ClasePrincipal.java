be06e1da6ff64243240f1a85f6d3df653ceb3192
Iván <56647887+Ivancrto@users.noreply.github.com>
2020-04-09 13:03:24 +0200

    Update ClasePrincipal.java

--- ClasePrincipal.java
+++ ClasePrincipal.java
@@ -2,6 +2,7 @@ import errors.MyErrorStrategy;
 import errors.TestE_Listener;
 import org.antlr.v4.runtime.*;
 
+import java.io.File;
 import java.io.IOException;
 
 /*
@@ -10,15 +11,23 @@ Sustituye Hello por el nombre del fichero que contiene la especificación de la 
 */
 public class ClasePrincipal {
     public static void main(String[] args) {
+
+
+        File file = new File("codico.c");
+
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         try{
 // Preparar el fichero de entrada para asignarlo al analizador léxico
             CharStream input = CharStreams.fromFileName(args[0]);
 // Crear el objeto correspondiente al analizador léxico con el fichero de entrada
             grupalLexer analex = new grupalLexer(input);
 
-            //PARA LOS ERRORES
-            //analex.removeErrorListeners ();
-            //analex.addErrorListener (new TestE_Listener.VerboseListener ());
+
 
 // Identificar al analizador léxico como fuente de tokens para el sintactico
             CommonTokenStream tokens = new CommonTokenStream (analex);
@@ -30,19 +39,7 @@ public class ClasePrincipal {
             anasint.addErrorListener (new TestE_Listener.VerboseListener ());
             anasint.setErrorHandler (new MyErrorStrategy ());
 
-            
-            //--------------------------------------------------------------------------------
 
-
-/*
-Si se quiere pasar al analizador algún objeto externo con el que trabajar, este deberá ser de una clase del mismo paquete
-Aquí se le llama "sintesis", pero puede ser cualquier nombre.
-HelloParser anasint = new HelloParser(tokens, new sintesis());
-*/
-/*
-Comenzar el análisis llamando al axioma de la gramática
-Atención, sustituye "AxiomaDeLaGramatica" por el nombre del axioma de tu gramática
-*/
             anasint.prg();
         } catch (org.antlr.v4.runtime.RecognitionException e) { //Fallo al reconocer la entrada
             System.err.println("REC " + e.getMessage());
